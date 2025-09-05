476005d524d1bd378ca3481562ee3e609b778c7a
MarieNyst <marie.anastacio@gmail.com>
2025-09-05 16:38:10 +0200

    simple model trains

--- src/utils/performancemodel.py
+++ src/utils/performancemodel.py
@@ -1,6 +1,11 @@
 import polars as pl
 from sklearn.ensemble import RandomForestRegressor
 
+## needed only for the test at the end of the file
+from sustainablecompetition.dataadaptors.csv_dataadaptor import CsvDataAdaptor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
 
 class PerformanceModel:
     """A performance model learning from 4 input files (currently, corresponding to the 4 tables in our future database)
@@ -12,61 +17,22 @@ class PerformanceModel:
         _type_: _description_
     """
     
-    def __init__(self, environments_path, instances_path, solvers_path, performances_path):
-        """Initialises the model with the given files, loads and preprocesses the data
-
+    def __init__(self):
+        """Initialises the model 
+        
         Args:
-            environments_path (str): path of the file containing the environment table
-            instances_path (str): path of the file containing the instance table
-            solvers_path (str): path of the file containing the solver table
-            performances_path (str): path of the file containing the performance table
+            should take the RF parameters
 
-        Raises:
-            FileNotFoundError: if csv files are not there
         """
-        self.environments_path = environments_path
-        self.instances_path = instances_path
-        self.solvers_path = solvers_path
-        self.performances_path = performances_path
-        self.load_data()
-        self.prepare_data()
         self.model = RandomForestRegressor(n_estimators=100, random_state=42)
 
-    def load_data(self):
-        """Reads the csv files and loads the data into memory
+    
 
-        Raises:
-            FileNotFoundError: if csv files are not there
-        """
-        # Load features
-        self.environments = pl.read_csv(self.environments_path)
-        self.instances = pl.read_csv(self.instances_path)
-        self.solvers = pl.read_csv(self.solvers_path)
-        # Load performances
-        self.perf = pl.read_csv(self.performances_path)
-        
-
-    def prepare_data(self):
-        """put all files together into X and Y
-        """
-        # Merge perf with environments on env_hash
-        merged = self.perf.join(self.environments, left_on="env_hash", right_on="env_hash", how="left")
-
-        # Merge with instances on inst_hash
-        merged = merged.join(self.instances, left_on="inst_hash", right_on="inst_hash", how="left")
-
-        # Merge with solvers on solver_hash
-        merged = merged.join(self.solvers, left_on="solver_hash", right_on="solver_hash", how="left")
-
-        # Drop hash columns and status, keep only features and perf
-        self.X = merged.drop(["env_hash", "inst_hash", "solver_hash", "status","perf"])
-        self.Y = self.perf["perf"].to_numpy()
-
-    def train(self):
+    def train(self, X,Y):
         """train the model
         for now very basic but could be better tailored in the future (handling saturated right tail for example)
         """
-        self.model.fit(self.X, self.Y)
+        self.model.fit(X, Y)
 
     def predict(self, x_input):
         """predicts stuff, for now simple but could be refined
@@ -86,11 +52,21 @@ class PerformanceModel:
 
 if __name__ == "__main__":
 
+    data = CsvDataAdaptor(pl.DataFrame())
+    data.get_performances("environments.csv", "instances.csv", "solvers.csv", "performances.csv")
+    # Drop hash columns and status, keep only features and perf
+    X = data.df.drop(["env_hash", "inst_hash", "solver_hash", "status","perf"])
+    # for this test, also drop non numerical data TODO handle them better
+    print(X.columns)
+    X = X.drop(["kernel_version","activated_modules","cpu_brand","cpu_model","solver_name"])
+    y_perfs = data.df["perf"].to_numpy()
+    y_status = data.df["status"].to_numpy()
     # Initialize
-    rf = PerformanceModel("environments.csv", "instances.csv", "solvers.csv", "performances.csv")
+    rf = PerformanceModel()
+    X_train, X_test, y_train, y_test = train_test_split(X, y_perfs, test_size=0.2, random_state=42)
+    rf.train(X_train, y_train)
+    y_pred = rf.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"MSE predict performance: {mse}")
 
-    # Train
-    rf.train()
 
-    # Predict
-    prediction = rf.predict()
