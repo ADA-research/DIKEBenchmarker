7e79f570b5559c54cdb8eb19bd2143ab6f98390b
Théo Matricon <theomatricon@gmail.com>
2025-09-09 10:29:50 +0200

    refactor into lambda consumer

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,10 +1,11 @@
 import asyncio
 import polars as pl
+from sustainablecompetition.benchmarkatoms import Result
 from sustainablecompetition.benchmarkingmethods.trivial_benchmarker import TrivialBenchmarker
 from sustainablecompetition.controller import Controller
-from sustainablecompetition.dataadapters.competition_dataadapter import CompetitionDataAdapter
-from sustainablecompetition.infrastructureadapters.virtual_runner import VirtualRunner
-from sustainablecompetition.resultconsumers.echo_consumer import EchoConsumer
+from sustainablecompetition.dataadaptors.competition_dataadaptor import CompetitionDataAdaptor
+from sustainablecompetition.infrastructureadaptors.virtual_runner import VirtualRunner
+from sustainablecompetition.resultconsumers.lambda_consumer import LambdaConsumer
 
 
 def test_run():
@@ -12,10 +13,12 @@ def test_run():
     BENCHMARK_IDS = [str(i) for i in range(NJOBS)]
     NSOLVERS = 10
     df = pl.DataFrame({"hash": BENCHMARK_IDS, **{str(solver_id): [int(solver_id)] * NJOBS for solver_id in range(NSOLVERS)}})
-    runner = VirtualRunner(CompetitionDataAdapter(df))
+    runner = VirtualRunner(CompetitionDataAdaptor(df))
     benchmarks = df.select("hash").to_series().to_list()
     columns = df.columns
     method = TrivialBenchmarker(benchmarks, columns[1])
-    consumer = EchoConsumer()
+    results: list[Result] = []
+    consumer = LambdaConsumer(results.append)
     controller = Controller(method, runner, njobs=1, consumers=[consumer])
     asyncio.run(controller.run())
+    assert len(results) == NJOBS
