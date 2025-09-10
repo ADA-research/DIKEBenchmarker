f2d5b313a98ef0444992a76d7c8e07240c35b970
Marie A. <marie.anastacio@gmail.com>
2025-09-10 14:03:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Configure Git filters for SQLite dump/restore
-git config filter.dumpsql.clean 'sqlite3 %f ".dump" > data/dump.sql'
-git config filter.dumpsql.smudge 'cat data/dump.sql | sqlite3 %f'
+git config filter.dumpsql.clean 'tmp=$(mktemp); cat > $tmp; sqlite3 $tmp .dump; rm $tmp'
+git config filter.dumpsql.smudge 'tmp=$(mktemp); sqlite3 $tmp; cat $tmp; rm $tmp'
 
 # Trigger the smudge filter to restore the database
 git checkout -- data/sustainablecompetition.db
